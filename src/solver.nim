bb9fa894926f90fcbbe3c32c797c99801ec783dc
JCallicoat <jordan.callicoat@gmail.com>
2026-07-01 18:41:19 -0500

    Fix edge case in solver

--- src/solver.nim
+++ src/solver.nim
@@ -1,14 +1,20 @@
+import std/algorithm
 import std/enumerate
 import std/sequtils
 import std/strformat
 import std/strutils
-
 import nim_wordle/word_list as wl
 import nim_wordle/valid_word_list as vwl
 
+type PossibleLetter = tuple[chr: char, pos: int]
 
-proc findCandidates(correct, possible, excluded: seq[char]) =
-  # echo &"correct: {correct}, possible: {possible}, excluded: {excluded}"
+proc parsePossible(s: string): seq[PossibleLetter] =
+  var i = 0
+  while i + 1 < s.len:
+    result.add((chr: s[i], pos: parseInt($s[i+1]) - 1))
+    i += 2
+
+proc findCandidates(correct: seq[char], possible: seq[PossibleLetter], excluded: seq[char]) =
   var candidates: seq[string] = @[]
   for word in wl.wordList.concat(vwl.validWordList):
     block inner:
@@ -20,32 +26,33 @@ proc findCandidates(correct, possible, excluded: seq[char]) =
           continue
         if word[i] != chr:
           break inner
-      for chr in possible:
-        if not (chr in word):
+      for p in possible:
+        if p.chr notin word:
+          break inner
+        if word[p.pos] == p.chr:
           break inner
       candidates.add(word)
+  candidates.sort()
   echo "Possible candidates:"
   for word in candidates:
     stdout.write(&"{word.toUpper} ")
   echo ""
 
-
 if isMainModule:
   var correct, possible, excluded: string = ""
-  stdout.write("Enter correct letters: ")
+  stdout.write("Enter correct letters (e.g. _e___): ")
   if not stdin.readLine(correct):
     stderr.writeLine("Correct list is needed")
     quit(1)
-
-  stdout.write("Enter possible letters: ")
+  if correct.len != 5:
+    stderr.writeLine("Correct must be exactly 5 characters")
+    quit(1)
+  stdout.write("Enter possible letters with positions (e.g. e2a4): ")
   if not stdin.readLine(possible):
     stderr.writeLine("Possible list is needed")
     quit(1)
-
   stdout.write("Enter excluded letters: ")
   if not stdin.readLine(excluded):
     stderr.writeLine("Excluded list is needed")
     quit(1)
-
-  findCandidates(correct.toLower.toSeq, possible.toLower.toSeq, excluded.toLower.toSeq)
-
+  findCandidates(correct.toLower.toSeq, parsePossible(possible.toLower), excluded.toLower.toSeq)
